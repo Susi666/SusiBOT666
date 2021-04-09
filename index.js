74e3d44d88685814a4fee5eaa4787aebd2b74fca
Susi666 <81292387+Susi666@users.noreply.github.com>
2021-04-09 17:47:50 -0600

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ CFonts.say('esubot\nWhatsApp Bot', {
   align: 'center',
   gradient: ['red', 'magenta']
 })
-CFonts.say(`'Samu330'`, {
+CFonts.say(`'SUSII'`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']
