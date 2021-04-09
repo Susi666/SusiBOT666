aab5b777a6ae82e75d931201c7d1fcbbd3d7b0d8
Susi666 <81292387+Susi666@users.noreply.github.com>
2021-04-09 12:12:37 -0600

    Update ip.js

--- plugins/ip.js
+++ plugins/ip.js
@@ -15,7 +15,7 @@ let handler = async(m, { conn, text, usedPrefix }) => {
 
 }
 
-handler.help = ['ip', 'ipcheck'].map(v => v + ' *ip_addr*')
+handler.help = ['ip', 'ipcheck'].map(v => v + ' ᴱˢᶜʳᶦᵇᵉ ᵘⁿᵃ ᶦᵖ')
 handler.tags = ['tools']
 handler.command = /^(ip|ipcheck)$/i
 handler.owner = false
